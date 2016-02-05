1d5b56e6ab4044fc712065f095b12cc8883edf3a
João Marcos Bizarro Lopes <jmbizarrolopes@gmail.com>
2016-02-05 10:57:11 -0200

    v0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'joaomarcos:format-print',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Use this package to format cpf/cnpj, phone and date',
   // URL to the Git repository containing the source code for this package.
